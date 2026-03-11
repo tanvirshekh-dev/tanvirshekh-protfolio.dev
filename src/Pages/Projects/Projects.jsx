7facc782a1fae015b2aeab106ae2fec0688eeae2
tanvirshekh-dev <tanvir@example.com>
2026-03-11 23:37:39 +0600

    added the json data for projects section

--- src/Pages/Projects/Projects.jsx
+++ src/Pages/Projects/Projects.jsx
@@ -1,19 +1,50 @@
-import { useLoaderData } from 'react-router'
+import { useLoaderData } from "react-router";
 
 const Projects = () => {
-    const projects = useLoaderData() ?? [];
+  const projects = useLoaderData() ?? [];
 
   return (
-    <div className='w-11/12 mx-auto'>
-      {
-        projects.map(project => (
-            <div key={project.id}>
-                <h2>{project.title}</h2>
+    <div>
+      <h2 className="text-center text-4xl mt-8 ">
+          My Recent{" "}
+          <span className="bg-gradient-to-r from-[#00d390] to-[#00fff2] bg-clip-text text-transparent">
+            Project
+          </span>
+        </h2>
+      <div className="w-11/12 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 mt-20 mb-28">
+        {projects.map((project) => (
+          <div key={project.id}>
+            <div className="card shadow-2xl">
+              <figure>
+                <img
+                  src={project.image}
+                  alt="Project Image"
+                  className="h-[200px] w-full"
+                />
+              </figure>
+              <div className="card-body">
+                <h2 className="card-title text-2xl">{project.title}</h2>
+                <p className="text-gray-400 text-md">
+                  {project.description}
+                </p>
+                {/* badge */}
+                <div className="flex gap-x-2 ">
+                  {
+                    project.technologies.map((tech, index)=>(
+                      <div key={index}>{tech}</div>
+                    ))
+                  }
+                </div>
+                <div className="card-actions justify-start">
+                  <button className="btn btn-primary">Buy Now</button>
+                </div>
+              </div>
             </div>
-        ))
-      }
+          </div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
