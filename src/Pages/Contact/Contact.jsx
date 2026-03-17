dc3e8e2c6bf2f66d5ce1c67a57b2a51d2009d5ab
Tanvir Shekh <tanvirshekh.dev@gmail.com>
2026-03-18 03:54:09 +0600

    make the contact us page

--- src/Pages/Contact/Contact.jsx
+++ src/Pages/Contact/Contact.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import { Linkedin, Mail } from "lucide-react";
+import { FaBehance } from "react-icons/fa";
+import { FiGithub } from "react-icons/fi";
 
 const Contact = () => {
   return (
@@ -9,30 +11,78 @@ const Contact = () => {
           Us
         </span>
       </h2>
-      <div className="w-11/12 mx-auto grid grid-cols-12 pt-16 mb-40">
+      <div className="w-11/12 mx-auto grid grid-cols-12 pt-16 mb-40 gap-x-10">
         {/* contact info */}
-        <div className="flex flex-col gap-y-5 col-span-5">
+        <div className="flex flex-col gap-y-10 col-span-4 bg-gray-800 px-10 py-10 rounded-xl cursor-pointer">
           {/* Email */}
           <div>
-            <h2 className="font-semibold text-gray-400 text-2xl">Email</h2>
-            <h5>tanvirshekh.dev@gmail.com</h5>
+            <h2 className="font-semibold text-gray-400 text-2xl border-l-2 h-5 flex items-center border-green-400 pl-2 mb-2">
+              Email
+            </h2>
+            <h5 className="text-gray-200">tanvirshekh.dev@gmail.com</h5>
           </div>
           {/* Location */}
           <div>
-            <h2 className="font-semibold text-gray-400 text-2xl">Location</h2>
-            <h5>Narayanganj, Dhaka, Bangladesh</h5>
+            <h2 className="font-semibold text-gray-400 text-2xl border-l-2 h-5 flex items-center border-green-400 pl-2 mb-2">
+              Location
+            </h2>
+            <h5 className="text-gray-200">Narayanganj, Dhaka, Bangladesh</h5>
           </div>
           {/* Phone */}
           <div>
-            <h2 className="font-semibold text-gray-400 text-2xl">Phone</h2>
-            <h5>+880 1595272589</h5>
+            <h2 className="font-semibold text-gray-400 text-2xl border-l-2 h-5 flex items-center border-green-400 pl-2 mb-2">
+              Phone
+            </h2>
+            <h5 className="text-gray-200">+880 1595272589</h5>
+            <h5 className="text-gray-200">+880 1618787709</h5>
+          </div>
+          {/* Social Media */}
+          <div>
+            <h2 className="font-semibold text-gray-400 text-2xl border-l-2 h-5 flex items-center border-green-400 pl-2 mb-2">
+              Socila Media
+            </h2>
+            {/* social links */}
+            <div className="py-4 flex gap-4">
+              <a
+                href="https://github.com/tanvirshekh-dev"
+                target="_blank"
+                className="inline-flex items-center justify-center w-12 h-12 rounded-md bg-gray-800 text-white border border-gray-500"
+              >
+                <FiGithub size={28} />
+              </a>
+
+              <a
+                href="https://www.linkedin.com/in/tanvirshekh/"
+                target="_blank"
+                className="inline-flex items-center justify-center w-12 h-12 rounded-md bg-gray-800 text-white border border-gray-500"
+              >
+                <Linkedin size={28} />
+              </a>
+
+              <a
+                href="mailto:tanvirshekh.dev@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center justify-center w-12 h-12 rounded-md bg-gray-800 text-white border border-gray-500 hover:bg-gray-700 transition-colors"
+              >
+                <Mail size={24} />
+              </a>
+
+              <a
+                href="https://www.behance.net/tanvirshekh-dev"
+                target="_blank"
+                className="inline-flex items-center justify-center w-12 h-12 rounded-md bg-gray-800 text-white border border-gray-500"
+              >
+                <FaBehance size={28} />
+              </a>
+            </div>
           </div>
         </div>
 
         {/* contact form */}
-        <div className="col-span-7">
+        <div className="col-span-8">
           <form>
-            <div>
+            <div className="bg-gray-800 px-8 py-10 rounded-xl cursor-pointer">
               <h2 className="text-3xl font-semibold text-gray-300 mb-2">
                 Get In Touch
               </h2>
@@ -41,14 +91,14 @@ const Contact = () => {
               <fieldset className="fieldset">
                 <input
                   type="text"
-                  className="input w-full mb-4"
+                  className="input w-full mb-2"
                   placeholder="Your Name"
                   required
                 />
               </fieldset>
               {/* email */}
               <legend className="fieldset-legend">Your Email</legend>
-              <label className="input validator w-full mb-4">
+              <label className="input validator w-full mb-2">
                 <svg
                   className="h-[1em] opacity-50"
                   xmlns="http://www.w3.org/2000/svg"
@@ -71,12 +121,13 @@ const Contact = () => {
                 Enter valid email address
               </div>
 
-              {/* your subject */}
+              {/* your massage */}
               <legend className="fieldset-legend">Your Massage</legend>
               <fieldset className="fieldset">
                 <textarea
-                  className="textarea h-40 mb-2 w-full"
+                  className="textarea h-30 mb-2 w-full"
                   placeholder="Tell Me About Your Project"
+                  required
                 ></textarea>
               </fieldset>
 
